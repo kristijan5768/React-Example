e1b5bfb942900438e75149e29d25ba48711d2f8e
kristijan5768 <kristijan5768@gmail.com>
2023-03-06 21:14:28 +0100

    feat:chuck norris task

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [godina, setGodina] = useState(2023);
-  const [klik, setKlik] = useState(0);
-  const init = useCallback(() => {
-    console.log("component init");
+  const [joke, setJoke] = useState("");
+
+  const getData = useCallback(async () => {
+    const response = await axios.get("https://api.chucknorris.io/jokes/random");
+    setJoke(response.data.value);
   }, []);
   useEffect(() => {
-    init();
-  }, [init]);
+    getData();
+  }, [getData]);
   return (
     <div className="">
-      <p>{godina}</p>
-      <button
-        onClick={() => {
-          setGodina(godina + 1);
-        }}
-      >
-        Dodaj broj
-      </button>
-      <p>{klik}</p>
-      <button
-        onClick={() => {
-          setKlik(klik + 1);
-        }}
-      >
-        Dodaj Klik
-      </button>
+      <p>{joke}</p>
+      <button onClick={getData}>RefreshJoke</button>
     </div>
   );
 }
